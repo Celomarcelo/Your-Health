6d723a017ac4ca7408ad3d5e5c31dd06f31a18f2
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-03-15 11:17:39 -0300

    update run.py
    created function that validate the age data from user

--- run.py
+++ run.py
@@ -4,6 +4,19 @@ def calculate_caloric_needs(age, weight, height, gender, activity_level):
     """
 
 age = int(input("Enter your age: "))
+def validate_age(age):
+    """
+    Function to validate age
+    """
+    try:
+        age = int(age)
+        if 18<= age <= 100:
+            raise ValueError("Age must be a positive integer between 18 and 100.")
+        return age
+    except ValueError:
+        print("Invalid input for age. Please enter a valid integer.")
+        return None
+    
 weight = float(input("Enter your weight in kg: "))
 height = float(input("Enter your height in meters: "))
 gender = input("Enter 'M' for male or 'F' for female: ").upper()
