bb10f8be39497eecf632f49b703a5b1d25363175
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-03-15 11:15:35 -0300

    update run.py
    cretated inputs for age, height,weight, activiti and gender

--- run.py
+++ run.py
@@ -2,3 +2,9 @@ def calculate_caloric_needs(age, weight, height, gender, activity_level):
     """
     Function that calculates calories like the Harris-Benedict Equation
     """
+
+age = int(input("Enter your age: "))
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+gender = input("Enter 'M' for male or 'F' for female: ").upper()
+activity_level = input("Enter your activity level (sedentary, light, moderate, active, very active): ").lower()
