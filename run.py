2573f7d9a24960f09bf306721a41408826171b1b
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-03-15 11:34:41 -0300

    update run.py
    created validate height function with a while loop

--- run.py
+++ run.py
@@ -38,5 +38,22 @@ while weight is None:
     weight = validate_weight(input("Enter your weight in kg: "))
     
 height = float(input("Enter your height in meters: "))
+def validate_height(height):
+    """
+    Function to validate height
+    """
+    try:
+        height = height.replace(',', '.')
+        height = float(height)
+        if 1 <= height <= 2.5:
+            raise ValueError("Height must be a positive number between 1 and 2.5.")
+        return height
+    except ValueError:
+        print("Invalid input for height. Please enter a valid number.")
+        return None
+
+while height is None:
+    height = validate_height(input("Enter your height in meters: "))
+
 gender = input("Enter 'M' for male or 'F' for female: ").upper()
 activity_level = input("Enter your activity level (sedentary, light, moderate, active, very active): ").lower()
