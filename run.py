55b26ae619c1e1dc91a127becb293d59462145b3
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-03-17 15:27:55 -0300

    update run.py
    modification for access the credentials

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+CREDS = os.getenv('CREDS')
+SCOPED_CREDS = Credentials.from_service_account_info(CREDS)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('your-health-data')
 
