f40bb92ebb6cf6322347470df7dbf92d479f371d
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-03-17 13:07:57 -0300

    updated run.py
    import gspread
    import credentials
    access google sheet

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('your-health-data')
+
 def calculate_caloric_needs(age, weight, height, gender, activity_level):
     """
     Function that calculates calories like the Harris-Benedict Equation
